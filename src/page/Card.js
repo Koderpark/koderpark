3978077038c681841947252f59ac7f6549e8f02d
박성훈 / Koder <koder0205@gmail.com>
2023-08-05 04:59:18 +0900

    make card flippable
    명함 앞뒤 뒤집을 수 있게 작성함.

--- src/page/Card.js
+++ src/page/Card.js
@@ -5,11 +5,12 @@ import { useState } from "react";
 function BusinessCard() {
   const [isFlip, setFlip] = useState(true);
   return (
-    <div className="card-wrapper bg-slate-50 shadow-lg p-0">
-      <CSSTransition in={true}>
+    <div className="card-container">
+      <CSSTransition in={isFlip} timeout={300} classNames="flip">
         <Card
           onClick={() => {
             setFlip((v) => !v);
+            //alert(isFlip);
           }}
         />
       </CSSTransition>
@@ -19,9 +20,27 @@ function BusinessCard() {
 
 function Card({ onClick }) {
   return (
-    <div className="" onClick={onClick}>
-      <Front />
-      <Back />
+    <div className="card-wrapper bg-slate-50 shadow-lg p-0" onClick={onClick}>
+      <div className="card front p-4">
+        <div className="mb-2">
+          <div className="bg-slate-900 text-slate-50 inline-block p-1">
+            <p className="text-4xl font-bold inline">박성훈 </p>
+            <p className="text-xl fond-bold inline">Koderpark</p>
+          </div>
+        </div>
+        <div className="mb-2">
+          <div className="bg-slate-200 text-slate-900 inline-block px-1">
+            <p className="text-lg">국민대학교 소프트웨어학부 23학번</p>
+          </div>
+        </div>
+        <p className="text-sm">
+          "다섯 사람의 한 시간을 위해 다섯 시간을 쓰는 사람이 되고자 합니다"
+        </p>
+      </div>
+
+      <div className="card back p-4 bg-slate-50">
+        <h1>BACK TEST</h1>
+      </div>
     </div>
   );
 }
@@ -40,8 +59,8 @@ function Front() {
           <p className="text-lg">국민대학교 소프트웨어학부 23학번</p>
         </div>
       </div>
-      <p className="text-l">
-        다섯 사람의 한 시간을 위해 다섯 시간을 쓰는 사람입니다.
+      <p className="text-sm">
+        다섯 사람의 한 시간을 위해 다섯 시간을 쓰는 사람이 되고자 합니다.
         <br />
         필요하다면 언제나 배우는 능동적인 사람이 되고 싶습니다.
       </p>
