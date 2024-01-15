59559f4bae3ebb871e7e7fc497b697ebf0ecc0ce
박성훈 / Koder <koder0205@gmail.com>
2024-01-16 04:50:24 +0900

    migration to nextjs - 1

--- src/images/social/index.js
+++
@@ -1,8 +0,0 @@
-import BOJ from "./boj.svg";
-import CF from "./cf.svg";
-import SOLVEDAC from "./solvedac.svg";
-import X from "./X.webp";
-import GITHUB from "./github.png";
-import KAKAOTALK from "./kakaotalk.png";
-
-export { BOJ, CF, SOLVEDAC, X, GITHUB, KAKAOTALK };
